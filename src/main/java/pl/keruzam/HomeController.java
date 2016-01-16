1a18b18ed44b272ff9f739d8bd4e30d9f506bdcf
Keruzam <mazurek.zg@gmail.com>
2016-01-16 21:23:55 +0100

    default thymeleaf loyout

--- src/main/java/pl/keruzam/HomeController.java
+++ src/main/java/pl/keruzam/HomeController.java
@@ -1,14 +1,19 @@
 package pl.keruzam;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
-	@RequestMapping("/status")
-	public String status() {
-		return "<h1>OK.</h2>";
+	@RequestMapping("/")
+	String index() {
+		return "index";
+	}
+
+	@RequestMapping("/login")
+	String login() {
+		return "login";
 	}
 
 }
